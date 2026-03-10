269623889f75a3e8ac8df711a73245c49ce33fa1
RevEng44 <anthony.delmastro@gmail.com>
2026-03-10 13:51:34 -0600

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -45,7 +45,7 @@ const Navbar = () => {
         <div className="flex justify-between h-20 items-center">
           <div className="flex items-center gap-4">
             <div className="w-16 h-16 bg-brand-green rounded overflow-hidden flex items-center justify-center shadow-lg border border-white/10">
-              <img src="/defender-logo.png" alt="Defender Logo" className="w-[80%] h-[80%] object-contain" />
+              <img src="/defender-logo.png" alt="Defender Logo" className="w-[85%] h-[85%] object-contain" />
             </div>
             <span className="text-white font-bold text-3xl tracking-tighter uppercase">Defender</span>
           </div>
@@ -218,8 +218,8 @@ const About = () => {
                 referrerPolicy="no-referrer"
               />
             </div>
-            <div className="absolute -bottom-12 -left-12 bg-brand-green rounded-3xl hidden md:block shadow-2xl border border-white/10 w-44 h-44 flex items-center justify-center">
-              <img src="/defender-logo.png" alt="Defender Logo" className="w-[80%] h-[80%] object-contain" />
+            <div className="absolute -bottom-12 -left-12 bg-brand-green rounded-3xl hidden md:flex items-center justify-center shadow-2xl border border-white/10 w-44 h-44">
+              <img src="/defender-logo.png" alt="Defender Logo" className="w-[85%] h-[85%] object-contain" />
             </div>
           </motion.div>
         </div>
